006fb725528da77755972636f66196fed33c2963
H1PEZECA <braabozeca@gmail.com>
2026-02-11 21:24:15 -0300

    Adiciona configuração do Vercel e implementa handler para Fastify

---
+++ api/api/index.ts
@@ -0,0 +1,24 @@
+import type { IncomingMessage, ServerResponse } from "node:http";
+import type { FastifyInstance } from "fastify";
+import { buildApp } from "../src/app.js";
+
+let appPromise: Promise<FastifyInstance> | undefined;
+
+async function getApp(): Promise<FastifyInstance> {
+  if (!appPromise) {
+    appPromise = (async () => {
+      const app = await buildApp();
+      await app.ready();
+      return app;
+    })();
+  }
+
+  return appPromise;
+}
+
+export default async function handler(req: IncomingMessage, res: ServerResponse): Promise<void> {
+  const app = await getApp();
+
+  // Encaminha a requisição HTTP da Vercel para o servidor do Fastify
+  app.server.emit("request", req, res);
+}
